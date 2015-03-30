9a830323999b2dc81c10efa412cf9b6b66571243
Nolan Amy <nolan@plickers.com>
2015-03-30 01:16:53 -0700

    Add Xcode Single View Application template

---
+++ RealmTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmTest
+//
+//  Created by Nolan Amy on 3/30/15.
+//  Copyright (c) 2015 Nolan Amy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealmTest
+//
+//  Created by Nolan Amy on 3/30/15.
+//  Copyright (c) 2015 Nolan Amy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
